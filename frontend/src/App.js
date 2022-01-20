44cebfcef5645804fe8bc68a46703593669fa00a
unknown <okwuosahpaschal@gmail.com>
2022-01-20 10:55:50 +0100

    fifth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Store from './store.js';
 import getBlockchain from "./ethereum";
 
 function App() {
@@ -14,9 +15,23 @@ function App() {
 
      init();
   }, []);
+
+  if(typeof window.ethereum === 'undefined') {
+    return (
+      <div className="container">
+        <div className="col-sm-12">
+          <h1>Blockchain Ecommerce App</h1>
+          <p>You need to install the latest version of Metamask</p>
+        </div>
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className="col-sm-12">
+        <h1>Blockchain Ecommerce App</h1>
+        <Store pay />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/store.js
@@ -0,0 +1,47 @@
+import { ethers } from "ethers";
+import axios from "axios";
+
+const API_URL = 'http://localhost:4000';
+
+const ITEMS = [
+    {
+        id: 1,
+        price: ethers.utils.parseEther('100'),
+    },
+    {
+        id: 2,
+        price: ethers.utils.parseEther('200'),
+    }
+]
+
+function Store({ paymentProcessor, dai }) {
+    const Buy = async item => {
+        const response1 = await axios.get(`${API_URL}/api/getPaymentId/${item.id}`);
+        const tx1 = await dai.approve(paymentProcessor.address, item.price);
+        await tx1.wait();
+
+        const res = await paymentProcessor.pay(item.price, response1.data.paymentId);
+    }
+    return (
+        <ul className="list-group">
+            <li className="list-group-item">
+                Buy item1 - <span className="front-weight-bold">100 DAI</span>
+                <button
+                   type="button"
+                   className="btn btn-primary float-right"
+                   onClick={() => buy(ITEMS[0])}>
+                       Buy
+                   </button>
+            </li>
+            <li className="list-group-item">
+                Buy item2 - <span className="front-weight-bold">200 DAI</span>
+                <button
+                   type="button"
+                   className="btn btn-primary float-right"
+                   onClick={() => buy(ITEMS[1])}>
+                       Buy
+                   </button>
+            </li>
+        </ul>
+    )
+}
