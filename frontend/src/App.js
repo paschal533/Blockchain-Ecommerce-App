60e1f8df724d912b2aaad80685dfb6bb7a78d35e
unknown <okwuosahpaschal@gmail.com>
2022-01-20 10:50:28 +0100

    fifth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import { useState, useEFFect } from "react";
+import { useState, useEffect } from "react";
 import getBlockchain from "./ethereum";
 
 function App() {
+  const [paymentProcessor, setPaymentProcessor] = useState(undefined);
+  const [dai, setDai] = useState(undefined);
+
+  useEffect(() => {
+     const init = async () => {
+        const { paymentProcessor, dai } = await getBlockchain()
+        setPaymentProcessor(paymentProcessor);
+        setDai(dai);
+     }
+
+     init();
+  }, []);
   return (
     <div className="App">
       
